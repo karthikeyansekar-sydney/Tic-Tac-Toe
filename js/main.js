9928380b3c33f4791f6323ba8e8304e6378d1d27
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-07-23 22:43:30 +1000

    Complete two-player game project

--- js/main.js
+++ js/main.js
@@ -2,8 +2,44 @@ const circle_class = 'playerO';
 const x_class = 'playerX';
 let y;
 /* Retrieves all the div tag */
+const restartButton = document.getElementById('restartButton')
 
+const squ = document.getElementById('grid-container');
+const winningMessageTextElement = document.getElementById('winningMessageText');
+const winningMessageElement = document.getElementById('winningMessage');
 const squares = document.querySelectorAll('.grid-container div');
+const squareArray = Array.from(squares);
+
+
+const resetGame = () => {
+  squares.forEach(square => {
+  square.classList.remove(x_class);
+  square.classList.remove(circle_class);
+  square.classList.add('playCards');
+    //cell.removeEventListener('click', handleClick)
+    //cell.addEventListener('click', handleClick, { once: true })
+  })
+  winningMessageElement.classList.remove('show')
+};
+
+restartButton.addEventListener('click', resetGame);
+
+
+const endGame = (draw, currentPlayer) => {
+  if(draw){
+winningMessageTextElement.innerText = "Draw";
+ } else {
+   winningMessageTextElement.innerText = `${currentPlayer}  Wins`;
+ }
+ winningMessageElement.classList.add('show');
+};
+
+const isDraw = () => {
+  return squareArray.every(cell => {
+    return cell.classList.contains(x_class) ||
+  cell.classList.contains(circle_class)
+  })
+};
 
 const winningCombinations = [
   [0, 1, 2],
@@ -26,10 +62,10 @@ function checkWin(currentClass){
   })
 }
 document.addEventListener('DOMContentLoaded', () => {
+  let currentPlayer = 'playerX';
   /* Here we are displaying the person who made the move */
   const playerDisplay = document.querySelector('#player');
-
-  let currentPlayer = 'playerX';
+  playerDisplay.innerHTML = currentPlayer;
 
 /* Here forEach method Iterates through all the nine div tags with parent class grid-container */
 /* array.forEach method accepts three arguments (value,index,arr) */
@@ -40,10 +76,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function clickOutcome(e) {
     /* converting into Array and storing it in squareArray */
-    const squareArray = Array.from(squares);
     const index = squareArray.indexOf(e.target);
 
-    playerDisplay.innerHTML = currentPlayer;
     var element = e.target;
   element.classList.remove("playCards");
 
@@ -59,19 +93,28 @@ document.addEventListener('DOMContentLoaded', () => {
     if (squareArray[index].classList.contains(x_class)) /*It returns false */
     {
       y = x_class;
+      if(checkWin(y))
+      {
+        endGame(false,currentPlayer);
+      }
+      else if (isDraw()) {
+    endGame(true)
+  }
       currentPlayer = 'playerO';
     }
     else
     {
       y= circle_class;
+      if(checkWin(y))
+      {
+        endGame(false,currentPlayer);
+      }
+      else if (isDraw()) {
+    endGame(true)
+  }
       currentPlayer = 'playerX';
 
     }
-    if(checkWin(y))
-    {
-      console.log('winner');
-    }
+
   }
 })
-
-//element.classList.contains("active");
